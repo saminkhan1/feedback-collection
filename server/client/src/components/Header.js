0107c5c5c094eb65a7b15c1c445ffb3acbf8e33a
Mozammil Khan <saminkhann1@gmail.com>
2019-06-12 09:35:30 -0400

    developing client side

---
+++ server/client/src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class  Header extends Component {
+    render() {
+        return (
+            <nav>
+            <div className="nav-wrapper">
+              <a href="/" className="left brand-logo">Emaily</a>
+              <ul id="nav-mobile" className="right">
+                <li><a>Login With Google</a></li>
+              </ul>
+            </div>
+          </nav>              
+        );
+    }
+}
+
+export default Header;
